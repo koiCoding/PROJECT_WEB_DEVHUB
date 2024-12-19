5412de347c5e92a64a0bfb0bc38d1180b78555e9
koiCoding <mkpha.contact@gmail.com>
2024-12-19 16:34:35 +0700

    Implementing OAuth Authentication - GitHub Provider

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,13 @@
-const Home = () => (
-  <h1 className="h1-bold">Welcome to the world of Next.js!!</h1>
-);
+import { auth } from "@/auth";
+
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+  return (
+    <>
+      <h1 className="h1-bold">Welcome to the world of Next.js!!</h1>
+    </>
+  );
+};
 
 export default Home;
