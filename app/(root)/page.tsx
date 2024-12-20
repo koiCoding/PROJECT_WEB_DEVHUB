023291d94a09a9990db18bbdb6d0acbd2efd80e1
koiCoding <mkpha.contact@gmail.com>
2024-12-20 10:07:12 +0700

    Complete Setup OAuth with GitHub & Google Provider

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
 
 const Home = async () => {
   const session = await auth();
@@ -6,6 +8,16 @@ const Home = async () => {
   return (
     <>
       <h1 className="h1-bold">Welcome to the world of Next.js!!</h1>
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">Log Out</Button>
+      </form>
     </>
   );
 };
